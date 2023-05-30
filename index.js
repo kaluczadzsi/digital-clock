70a3861bb739b52bd1e66809e15960604a49faf2
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-05-30 18:49:53 +0200

    Project completed

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 'use strict';
 
-const hour = document.querySelector('.clock__hours-content');
-const minute = document.querySelector('.clock__minutes-content');
-const second = document.querySelector('.clock__seconds-content');
-const ampm = document.querySelector('.clock__ampm-text');
+const clockNumbers = document.querySelectorAll('h2');
+const hourEl = document.querySelector('.clock__hours-content');
+const minuteEl = document.querySelector('.clock__minutes-content');
+const secondEl = document.querySelector('.clock__seconds-content');
+const ampmEl = document.querySelector('.clock__ampm-text');
+
+function updateClock() {
+  clockNumbers.forEach((number) => {
+    number.classList.remove('hidden');
+  });
+
+  const date = new Date();
+  const currentHour = date.getHours();
+  const currentMinute = date.getMinutes();
+  const currentSecond = date.getSeconds();
+  const currentAmPm = hourEl.innerText > 12 ? 'PM' : 'AM';
+
+  hourEl.innerText = currentHour;
+  minuteEl.innerText = currentMinute;
+  secondEl.innerText = currentSecond;
+  ampmEl.innerText = currentAmPm;
+
+  secondEl.innerText =
+    secondEl.innerText < 10 ? `0${secondEl.innerText}` : secondEl.innerText;
+}
+
+window.addEventListener('load', updateClock);
+setInterval(updateClock, 1000);
