269cee7bc0366d1903c1891a913cfc0684d5f6f5
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-05-30 18:15:06 +0200

    last clock child styles added, JS file too

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const hour = document.querySelector('.clock__hours-content');
+const minute = document.querySelector('.clock__minutes-content');
+const second = document.querySelector('.clock__seconds-content');
+const ampm = document.querySelector('.clock__ampm-text');
